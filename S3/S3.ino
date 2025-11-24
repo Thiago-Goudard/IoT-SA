233408b98a574872b6031803a4a2dc3340b8a592
Thiago Thierry Goudard <thiago_goudard@estudante.sesisenai.org.br>
2025-11-24 08:18:39 -0300

    ajuste no cod

--- S3/S3.ino
+++ S3/S3.ino
@@ -1,157 +1,148 @@
-<<<<<<< HEAD
-#include <WiFi.h>
 #include <WiFiClientSecure.h>
 #include <PubSubClient.h>
+#include <WiFi.h>
 #include <ESP32Servo.h>
 
-// ========================
-//  WIFI – S3
-// ========================
+#define PINO_LED 2
+#define TRIG 26
+#define ECHO 25
+#define PINO_SERVO1 19
+#define PINO_SERVO2 18
+#define PINO_PRESENCA 14
+
+WiFiClientSecure client;
+PubSubClient mqtt(client);
+
+Servo servo3;
+Servo servo4;
+
 const char* SSID = "FIESC_IOT_EDU";
 const char* PASS = "8120gv08";
 
-// ========================
-//  MQTT – HiveMQ Cloud
-// ========================
-const char* MQTT_SERVER = "810a9479164b4b4d81ba1c4879369294.s1.eu.hivemq.cloud";
-const int   MQTT_PORT   = 8883;
-const char* MQTT_USER   = "Placa_3";
-const char* MQTT_PASS   = "Placa123";
+const char* BROKER_URL  = "810a9479164b4b4d81ba1c4879369294.s1.eu.hivemq.cloud";
+const int   BROKER_PORT = 8883;
+const char* BROKER_USER = "Placa_3";
+const char* BROKER_PASS = "Placa123";
 
-WiFiClientSecure secureClient;
-PubSubClient mqtt(secureClient);
+const char* TOPIC_PUBLISH_PRESENCA = "Projeto S3 Presenca3";
+const char* TOPIC_PUBLISH_OBJETO   = "Projeto S3 Ultrassom3";
 
-// ========================
-//  PINOS DO S3
-// ========================
-#define TRIG_PIN 12
-#define ECHO_PIN 13
+const char* TOPICO_SUBSCRIBE = "S1 iluminacao";
 
-#define LED_PIN 25
-#define SERVO1_PIN 32
-#define SERVO2_PIN 33
+const char* TOPIC_PUBLISH_1 = "Projeto S2 Distancia1";
+const char* TOPIC_PUBLISH_2 = "Projeto S2 Distancia2";
 
-Servo servo1;
-Servo servo2;
+unsigned long lastPublish = 0;
+int publishInterval = 3000;
 
-bool estadoPresenca = false;
+long medirDistancia(int trigPin, int echoPin) {
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
 
-// =========================================================
-//  CALLBACK – RECEBE COMANDOS MQTT
-// =========================================================
+  long duracao = pulseIn(echoPin, HIGH, 30000);
+  long distancia = (duracao * 0.034) / 2;
+
+  return distancia;
+}
+
 void callback(char* topic, byte* payload, unsigned int length) {
-    String msg = "";
-    for (int i = 0; i < length; i++) msg += (char)payload[i];
+  String mensagem;
 
-    if (String(topic) == "S1/Iluminacao") {
-        if (msg == "Escuro") digitalWrite(LED_PIN, HIGH);
-        else digitalWrite(LED_PIN, LOW);
+  for (int i = 0; i < length; i++) {
+    mensagem += (char)payload[i];
+  }
+
+  if (mensagem == "acender") {
+    digitalWrite(PINO_LED, HIGH);
+  } 
+  else if (mensagem == "apagar") {
+    digitalWrite(PINO_LED, LOW);
+  } 
+  else if (String(topic) == TOPIC_PUBLISH_1) {
+    if (mensagem == "objeto_proximo") {
+      servo3.write(90);
+    } else if (mensagem == "objeto_longe") {
+      servo3.write(45);
     }
+  } 
+  else if (String(topic) == TOPIC_PUBLISH_2) {
+    if (mensagem == "objeto_proximo") {
+      servo4.write(90);
+    } else if (mensagem == "objeto_longe") {
+      servo4.write(45);
+    }
+  }
 
-    if (String(topic) == "S3/Servo1") servo1.write(msg.toInt());
-    if (String(topic) == "S3/Servo2") servo2.write(msg.toInt());
+  Serial.println(mensagem);
 }
 
-// =========================================================
-//  CONECTAR AO MQTT
-// =========================================================
+void conectarWiFi() {
+  WiFi.begin(SSID, PASS);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(300);
+  }
+}
+
 void conectarMQTT() {
-    while (!mqtt.connected()) {
-        String id = "S3-" + String(random(9999));
-        mqtt.connect(id.c_str(), MQTT_USER, MQTT_PASS);
-        delay(300);
+  mqtt.setServer(BROKER_URL, BROKER_PORT);
+  client.setInsecure();
+  mqtt.setCallback(callback);
+
+  while (!mqtt.connected()) {
+    String clientId = "S3_" + String(random(0xffff), HEX);
+
+    if (mqtt.connect(clientId.c_str(), BROKER_USER, BROKER_PASS)) {
+      mqtt.subscribe(TOPICO_SUBSCRIBE);
+      mqtt.subscribe(TOPIC_PUBLISH_1);
+      mqtt.subscribe(TOPIC_PUBLISH_2);
+      mqtt.subscribe("Projeto/S3/Controle");
+    } 
+    else {
+      delay(1500);
     }
-
-    mqtt.subscribe("S1/Iluminacao");
-    mqtt.subscribe("S3/Servo1");
-    mqtt.subscribe("S3/Servo2");
+  }
 }
 
-// =========================================================
-//  SENSOR ULTRASSÔNICO
-// =========================================================
-float distanciaCM() {
-    digitalWrite(TRIG_PIN, LOW);
-    delayMicroseconds(2);
-    digitalWrite(TRIG_PIN, HIGH);
-    delayMicroseconds(10);
-    digitalWrite(TRIG_PIN, LOW);
-
-    long duracao = pulseIn(ECHO_PIN, HIGH, 30000);
-    return duracao * 0.0343 / 2;
-}
-
-// =========================================================
-//  SETUP
-// =========================================================
 void setup() {
-    Serial.begin(115200);
+  Serial.begin(115200);
 
-    pinMode(TRIG_PIN, OUTPUT);
-    pinMode(ECHO_PIN, INPUT);
-    pinMode(LED_PIN, OUTPUT);
+  pinMode(PINO_LED, OUTPUT);
+  pinMode(PINO_PRESENCA, INPUT);
+  pinMode(TRIG, OUTPUT);
+  pinMode(ECHO, INPUT);
 
-    servo1.setPeriodHertz(50);
-    servo2.setPeriodHertz(50);
-    servo1.attach(SERVO1_PIN, 500, 2400);
-    servo2.attach(SERVO2_PIN, 500, 2400);
+  servo3.attach(PINO_SERVO1);
+  servo4.attach(PINO_SERVO2);
+  servo3.write(0);
+  servo4.write(0);
 
-    WiFi.begin(SSID, PASS);
-    while (WiFi.status() != WL_CONNECTED) delay(300);
-
-    secureClient.setInsecure();
-    mqtt.setServer(MQTT_SERVER, MQTT_PORT);
-    mqtt.setCallback(callback);
-
-    conectarMQTT();
-}
-
-// =========================================================
-//  LOOP PRINCIPAL
-// =========================================================
-void loop() {
-    if (!mqtt.connected()) conectarMQTT();
-    mqtt.loop();
-
-    float dist = distanciaCM();
-
-    // ---------- PRESENÇA DETECTADA ----------
-    if (dist > 0 && dist < 10 && !estadoPresenca) {
-        estadoPresenca = true;
-
-        mqtt.publish("S3/Presenca", "1");
-        mqtt.publish("S3/Status", "Presenca detectada - Servos acionados");
-
-        Serial.println(">>> PRESENÇA DETECTADA!");
-
-        servo1.write(180);
-        servo2.write(0);
-    }
-
-    // ---------- ÁREA LIVRE ----------
-    if ((dist >= 10 || dist == 0) && estadoPresenca) {
-        estadoPresenca = false;
-
-        mqtt.publish("S3/Presenca", "0");
-        mqtt.publish("S3/Status", "Area livre - Servos retornaram");
-
-        Serial.println(">>> ÁREA LIVRE!");
-
-        servo1.write(0);
-        servo2.write(180);
-    }
-
-    delay(200);
-}
-
-=======
-void setup() {
-  // put your setup code here, to run once:
-
+  conectarWiFi();
+  conectarMQTT();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  if (!mqtt.connected()) conectarMQTT();
+  mqtt.loop();
 
+  long distancia = medirDistancia(TRIG, ECHO);
+  Serial.println(distancia);
+
+  if (distancia > 0 && distancia < 10) {
+    mqtt.publish(TOPIC_PUBLISH_1, "objeto_proximo");
+  } 
+  else if (distancia > 10) {
+    mqtt.publish(TOPIC_PUBLISH_2, "objeto_longe");
+  }
+
+  unsigned long agora = millis();
+  if (agora - lastPublish >= publishInterval) {
+    lastPublish = agora;
+    int presenca = digitalRead(PINO_PRESENCA);
+    mqtt.publish(TOPIC_PUBLISH_PRESENCA, String(presenca).c_str());
+  }
+
+  delay(20);
 }
->>>>>>> 9cb20efe7589eac87c00975feeb3d16119eb70b6
