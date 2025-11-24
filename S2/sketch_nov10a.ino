4a0a97d5dd92f48970e50ebd053844f6e45cfd1c
ViniciusTn <vinicius_t_nunes@estudante.sesisenai.org.br>
2025-11-24 09:02:19 -0300

    add - explicação
    Added comments for clarity and understanding of the code.

--- S2/sketch_nov10a.ino
+++ S2/sketch_nov10a.ino
@@ -1,13 +1,33 @@
+/* Bibliotecas usadas
+WiFi.h → permite conectar o ESP32 ao Wi-Fi.
+WiFiClientSecure.h → cria uma conexão segura (SSL/TLS), essencial para HiveMQ Cloud.
+PubSubClient.h → biblioteca cliente MQTT, envia e recebe mensagens.*/
+
 #include <WiFi.h>
 #include <WiFiClientSecure.h>
 #include <PubSubClient.h>
 
+
+/*Objetos principais
+client → conexão segura.
+mqtt → cliente MQTT usando o client seguro.*/
+
 WiFiClientSecure client;
 PubSubClient mqtt(client);
+
+/*Configurações de Wi-Fi
+Credenciais para conectar o ESP32 à rede.*/
 
 // Wi-Fi
 const char* SSID = "FIESC_IOT_EDU";
 const char* WIFI_PASS = "8120gv08";
+
+
+/*Configurações do HiveMQ Cloud
+Servidor MQTT público em nuvem.
+Porta 8883 → conexão segura (SSL).
+Usuário e senha para autenticação.*/
+
 
 // HiveMQ Cloud
 const char* MQTT_SERVER = "810a9479164b4b4d81ba1c4879369294.s1.eu.hivemq.cloud";
@@ -15,9 +35,18 @@ const int MQTT_PORT = 8883;
 const char* MQTT_USER = "Placa_2";
 const char* MQTT_PASS = "Placa123";
 
+/*Tópicos MQTT 
+sensor/distancia → ESP32 publica mensagens.
+sensor/comando → ESP32 escuta comandos (acender/apagar).*/
+
 // Tópicos MQTT
 const char* TOPICO_SENSOR = "sensor/distancia";
 const char* TOPICO_LED = "sensor/comando";
+
+/*trigPin → envia pulso ultrassônico
+echoPin → recebe de volta o sinal
+distancia → armazena a distância calculada
+detectou → impede mandar mensagens repetidas*/
 
 // Sensor ultrassônico
 const int trigPin = 9;
@@ -26,7 +55,18 @@ long duracao;
 float distancia;
 bool detectou = false;
 
+/*Usa o LED interno da placa do ESP32.*/
+
 #define PINO_LED LED_BUILTIN
+
+/*Essa função é chamada automaticamente quando alguma mensagem chega no tópico sensor/comando.
+O que ela faz:
+Converte o payload em uma String.
+Mostra no Serial Monitor o tópico e a mensagem.
+Se a mensagem for:
+"acender" → liga o LED
+"apagar" → desliga o LED*/
+
 
 void callback(char* topic, byte* payload, unsigned int length) {
   String mensagem;
@@ -45,6 +85,8 @@ void callback(char* topic, byte* payload, unsigned int length) {
     digitalWrite(PINO_LED, LOW);
   }
 }
+
+/*Configura pinos*/
 
 void setup() {
   pinMode(PINO_LED, OUTPUT);
@@ -117,6 +159,8 @@ void loop() {
     Serial.println("Área livre");
   }
 
+  /*Mantém conexão viva*/
+  
   mqtt.loop();
   delay(300);
 }
